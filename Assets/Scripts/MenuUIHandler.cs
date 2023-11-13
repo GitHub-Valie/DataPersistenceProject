89b00730edf9bbb36d27bca88489f3d597376182
ValentinLHB <valentin.lhb@gmail.com>
2023-11-13 15:06:47 +0100

    basic scene flow + UI

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1); // Main scene #1
+    }
+
+    public void ToHighScores()
+    {
+        SceneManager.LoadScene(2); // High score scene #2
+    }
+
+    public void Exit()
+    {
+        /* Exit using conditional compiling */
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit(); // original code to quit Unity player
+        #endif
+    }
+}
